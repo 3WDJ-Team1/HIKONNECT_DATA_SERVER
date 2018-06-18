4551672be5dd12db5e2c40e266b28fa293ddb549
SongSol <thdthf159@naver.com>
2018-06-18 15:49:23 +0900

    location_memo update (no picture)

--- routes/location.js
+++ routes/location.js
@@ -1,7 +1,6 @@
 var express      = require('express');
 var router       = express.Router();
 var mysql        = require('mysql');
-var multer       = require('multer');
 var fs           = require('fs');
 var multiparty   = require('multiparty');
 require('date-utils');
@@ -18,24 +17,21 @@ var connection = mysql.createConnection({
 });
 
 router.post('/regLocation',function (req, res) {
-    var data = {};
-    var form = new multiparty.Form({
-        autoFiles: true,
-        uploadDir: 'public/images/LocationMemo'
-    });
-    var fields = [];
-
-    form.parse(req, function (err, fields, files) {
+    // Get current time.
+    newDate = new Date();
+    time = newDate.toFormat('YYYY-MM-DD HH24:MI:SS');
+    if (req.body.picture == 'false') {
+        var data = {};
         data = {
-            'schedule_no'       : fields.schedule_no[0],
-            'title'             : fields.title[0],
-            'content'           : fields.content[0],
-            'writer'            : fields.writer[0],
-            'picture'           : fields.picture[0],
+            'schedule_no'       : req.body.schedule_no,
+            'title'             : req.body.title,
+            'content'           : req.body.content,
+            'writer'            : req.body.writer,
+            'picture'           : req.body.picture,
             'created_at'        : time,
             'updated_at'        : time,
-            'latitude'          : fields.latitude[0],
-            'longitude'         : fields.longitude[0]
+            'latitude'          : req.body.latitude,
+            'longitude'         : req.body.longitude
         };
         connection.query('insert into location_memo SET ?', data, function (err, rows) {
             if (err == undefined)
@@ -43,18 +39,43 @@ router.post('/regLocation',function (req, res) {
             else
                 console.log(err);
         });
-        connection.query('select * from location_memo where created_at = ?', time, function (err, rows) {
-            var filename = `${rows[0]['no']}_${rows[0]['writer']}`;
-            fs.rename(files.location[0].path, 'public/images/LocationMemo/' + filename + '.jpg', function (err) {
-                if (err) throw err;
-                console.log('Success');
+    }
+    else {
+        var data = {};
+        var form = new multiparty.Form({
+            autoFiles: true,
+            uploadDir: 'public/images/LocationMemo'
+        });
+        var fields = [];
+        form.parse(req, function (err, fields, files) {
+            data = {
+                'schedule_no'       : fields.schedule_no[0],
+                'title'             : fields.title[0],
+                'content'           : fields.content[0],
+                'writer'            : fields.writer[0],
+                'picture'           : fields.picture[0],
+                'created_at'        : time,
+                'updated_at'        : time,
+                'latitude'          : fields.latitude[0],
+                'longitude'         : fields.longitude[0]
+            };
+            connection.query('insert into location_memo SET ?', data, function (err, rows) {
+                if (err == undefined)
+                    res.send('Success');
+                else
+                    console.log(err);
+            });
+            connection.query('select * from location_memo where created_at = ?', time, function (err, rows) {
+                var filename = `${rows[0]['no']}_${rows[0]['writer']}`;
+                fs.rename(files.location[0].path, 'public/images/LocationMemo/' + filename + '.jpg', function (err) {
+                    if (err) throw err;
+                    console.log('Success');
+                });
             });
         });
-    });
-    // Get current time.
-    newDate = new Date();
-    time = newDate.toFormat('YYYY-MM-DD HH24:MI:SS');
+    }
 });
+
 
 router.post('/getLocationPic', function (req, res) {
     console.log(req);
